883bcb2a4aa7bce755f107c7bf8f1f2dbc4b797f
Ming Huo <Ming.Huo@disney.com>
2016-01-02 00:15:12 -0800

    add hello and change server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express();
 app.use(express.static(__dirname+'/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port, ipaddress);
