03337556259fa12a9f20cff30c16ad0cf3bc97ce
Ming Huo <ming.huo@gmail.com>
2016-01-20 21:24:47 -0800

    first commit -  change server file

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ app.use(express.static(__dirname+'/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+/*
+app.get('/', function(req, res){
+    res.send('hello world --- cs5610');
+});
+*/
+
 app.listen(port, ipaddress);
