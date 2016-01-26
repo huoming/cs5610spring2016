9dc3109d2fceb044630fe8d43bd9e87cb0176924
Ming Huo <ming.huo@gmail.com>
2016-01-25 22:38:33 -0800

    add in assignment folder

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname+'/public'));
+
+app.use(express.static(__dirname+'/public_two'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-/*
-app.get('/', function(req, res){
-    res.send('hello world --- cs5610');
-});
-*/
+
+/*app.get('/helloWorld', function(req, res){
+    res.send('hello world still me!');
+});*/
 
 app.listen(port, ipaddress);
